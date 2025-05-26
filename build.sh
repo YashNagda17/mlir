b26dc9e5c4607e5014fed3774bbeeb568915f6f9
Ondřej Čertík <ondrej@certik.us>
2025-05-26 18:44:29 +0200

    Compile all at once

--- build.sh
+++ build.sh
@@ -5,9 +5,5 @@ set -ex
 CFLAGS="-fsanitize=address -g -Wall -ferror-limit=1"
 
 re2c -b tokenizer.re -o tokenizer.c
-clang $CFLAGS -c tokenizer.c -o tokenizer.o
-clang $CFLAGS -c arena.c -o arena.o
-clang $CFLAGS -c mlir_parser.c -o mlir_parser.o
-clang $CFLAGS -c parser.c -o parser.o
-clang $CFLAGS -o parser parser.o tokenizer.o arena.o mlir_parser.o
+clang $CFLAGS -o parser parser.c tokenizer.c arena.c mlir_parser.c
 ./parser
