bd19fbe8485eb685cf9a3393139db4811bbee967
Ondřej Čertík <ondrej@certik.us>
2025-05-27 20:42:57 +0200

    Works

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ CFLAGS="-fsanitize=address -g -Wall -ferror-limit=1"
 clang $CFLAGS -I. -o test_format tests/test_format.c base/arena.c base/string.c base/format.c
 ./test_format
 
-#re2c -b tokenizer.re -o tokenizer.c
-#clang $CFLAGS -I. -o parser parser.c tokenizer.c base/arena.c mlir_parser.c
-#./parser
+re2c -b tokenizer.re -o tokenizer.c
+clang $CFLAGS -I. -o parser parser.c tokenizer.c base/arena.c mlir_parser.c
+./parser
