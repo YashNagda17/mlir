c7c1a1e88a528b1aa73d5c431616fe43944dd5e7
Ondřej Čertík <ondrej@certik.us>
2025-05-27 20:47:21 +0200

    X

---
+++ base/io.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include <base/arena.h>
+#include <base/string.h>
+
+// Returns the file contents as a null-terminated string in `text`.
+// Returns `true` on success, otherwise `false`.
+bool read_file(Arena *arena, const string filename, string *text);
+string read_file_ok(Arena *arena, const string filename);
