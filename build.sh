57ce13c5fc21a9e547d10064f033556d43a22ce1
Ondřej Čertík <ondrej@certik.us>
2025-05-28 18:58:19 +0200

    Compile everything

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ clang $CFLAGS -I. -o run_tests tests/run_tests.c base/arena.c base/string.c base
 ./run_tests
 
 re2c -b tokenizer.re -o tokenizer.c
-clang $CFLAGS -I. -o parser parser.c tokenizer.c base/arena.c base/io.c mlir_parser.c
+clang $CFLAGS -I. -o parser parser.c tokenizer.c mlir_parser.c base/arena.c base/string.c base/format.c base/io.c
 ./parser
--- mlir_parser.c
+++ mlir_parser.c
@@ -3,34 +3,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <base/string.h>
+
 #include "mlir_parser.h"
-
-string str_from_cstr_view(char *cstr) {
-    return (string){.str=cstr, .size=strlen(cstr)-1};
-}
-
-string str_from_cstr_len_view(char *cstr, uint64_t size) {
-    return (string){.str=cstr, .size=size};
-}
-
-char *str_to_cstr_copy(Arena *arena, string str) {
-    char *cstr = arena_alloc_array(arena, char, str.size+1);
-    memcpy(cstr, str.str, str.size);
-    cstr[str.size] = '\0';
-    return cstr;
-}
-
-bool str_eq(string a, string b) {
-    if (a.size == b.size) {
-        return (memcmp(a.str, b.str, a.size) == 0);
-    } else {
-        return false;
-    }
-}
-
-string str_substr(string str, uint64_t min, uint64_t size) {
-    return (string){.str=str.str+min, .size=size};
-}
 
 
 void parser_error(Arena *arena, string msg, uint64_t first, uint64_t last) {
