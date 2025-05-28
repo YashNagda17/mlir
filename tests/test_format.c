29e9dd94ba3e16a8631b505331100bf3ca9ad7f3
Ondřej Čertík <ondrej@certik.us>
2025-05-28 17:13:04 +0200

    Add tests

--- tests/test_format.c
+++ tests/test_format.c
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <ctype.h>
 #include <stdarg.h>
+#include <assert.h>
 
 #include <base/format.h>
 
@@ -20,30 +21,36 @@ int main() {
     // Example with no arguments
     string fmt = str_lit("Hello!");
     string result = format(arena, fmt);
+    assert(str_eq(result, str_lit("Hello!")));
     printf("No args: %.*s\n", (int)result.size, result.str);
 
     // Example with one argument
     fmt = str_lit("Hello, {}!");
     result = format(arena, fmt, "world");
+    assert(str_eq(result, str_lit("Hello, world!")));
     printf("One arg: %.*s\n", (int)result.size, result.str);
 
     fmt = str_lit("Hello, {}!");
     result = format(arena, fmt, 5);
+    assert(str_eq(result, str_lit("Hello, 5!")));
     printf("No args: %.*s\n", (int)result.size, result.str);
 
     // Example with formatted double
     fmt = str_lit("Value: {:10.5f}");
     result = format(arena, fmt, pi);
+    assert(str_eq(result, str_lit("Value:    3.14159")));
     printf("Formatted double: %.*s\n", (int)result.size, result.str);
 
     // Example with formatted char
     fmt = str_lit("Char: {:^5}");
     result = format(arena, fmt, 'x');
+    //assert(str_eq(result, str_lit("Char:  120")));
     printf("Formatted char: %.*s\n", (int)result.size, result.str);
 
     // Example with multiple arguments
     fmt = str_lit("Hello, {}, {}, {}, {}!");
-    result = format(arena, fmt, "world", 35.5, 3, fmt);
+    string t = str_lit("XX");
+    result = format(arena, fmt, "world", 35.5, t, 3);
     printf("Multiple args: %.*s\n", (int)result.size, result.str);
 
     arena_free(arena);
