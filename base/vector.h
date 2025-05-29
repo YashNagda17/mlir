c478f89549e7d1e1caaacb9023b94eb0d84b43a7
Ondřej Čertík <ondrej@certik.us>
2025-05-29 15:41:07 +0200

    Use T

--- base/vector.h
+++ base/vector.h
@@ -6,17 +6,17 @@
 static int vec_called_const = 0xdeadbeef;
 #endif
 
-#define VecType int64_t
+#define T int64_t
 
-typedef struct {
+typedef struct vector_##T {
     VecType* p;
     size_t n, max;
 #ifdef WITH_BASE_ASSERT
     int reserve_called;
 #endif
-} vector_VecType;
+} vector_##T;
 
-void vector_VecType_reserve(Arena *arena, vector_VecType vec, size_t max); 
-void vector_VecType_push_back(Arena *arena, vector_VecType vec, VecType x); 
+void vector_##T##_reserve(Arena *arena, vector_##T vec, size_t max); 
+void vector_##T##(Arena *arena, vector_##T vec, T x); 
 
-#undef VecType
+#undef T
