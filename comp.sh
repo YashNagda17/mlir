99e3458d6171720140b41cd5ff168cb48ef8ae03
Ondřej Čertík <ondrej@certik.us>
2025-09-05 23:04:55 -0600

    All tests working (#27)

--- comp.sh
+++ comp.sh
@@ -29,20 +29,48 @@ TEST=chunked_cross_entropy_forward
 ./parser --classic tests/${TEST}.ttir > ${TEST}2.ttir
 diff -Naur tests/${TEST}.ttir ${TEST}2.ttir
 
-#b.mlir
-#c.mlir
+TEST=b
+./parser --classic tests/${TEST}.mlir > ${TEST}2.mlir
+diff -Naur tests/${TEST}.mlir ${TEST}2.mlir
+
+TEST=c
+./parser --classic tests/${TEST}.mlir > ${TEST}2.mlir
+diff -Naur tests/${TEST}.mlir ${TEST}2.mlir
 
 TEST=add_kernel
 ./parser --classic tests/${TEST}.ttir > ${TEST}2.ttir
 diff -Naur tests/${TEST}.ttir ${TEST}2.ttir
 
-#d.mlir
-#add1.ttir
-#t3.mlir
-#t2.mlir
-#simple.mlir
-#t1.mlir
-#a.mlir
+# Hand modified:
+#TEST=d
+#./parser --classic tests/${TEST}.mlir > ${TEST}2.mlir
+#diff -Naur tests/${TEST}.mlir ${TEST}2.mlir
 
+# Hand modified:
+#TEST=add1
+#./parser --classic tests/${TEST}.ttir > ${TEST}2.ttir
+#diff -Naur tests/${TEST}.ttir ${TEST}2.ttir
 
+# Weird indenting
+#TEST=t3
+#./parser --classic tests/${TEST}.mlir > ${TEST}2.mlir
+#diff -Naur tests/${TEST}.mlir ${TEST}2.mlir
 
+# Weird indenting
+#TEST=t2
+#./parser --classic tests/${TEST}.mlir > ${TEST}2.mlir
+#diff -Naur tests/${TEST}.mlir ${TEST}2.mlir
+
+TEST=simple
+./parser --classic tests/${TEST}.mlir > ${TEST}2.mlir
+diff -Naur tests/${TEST}.mlir ${TEST}2.mlir
+
+# Weird formatting
+#TEST=t1
+#./parser --classic tests/${TEST}.mlir > ${TEST}2.mlir
+#diff -Naur tests/${TEST}.mlir ${TEST}2.mlir
+
+# Generic printing (not classic)
+#TEST=a
+#./parser --classic tests/${TEST}.mlir > ${TEST}2.mlir
+#diff -Naur tests/${TEST}.mlir ${TEST}2.mlir
